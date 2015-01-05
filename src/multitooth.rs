018dacae434ed00585953ee690d697a82a2c91b8
Richo Healey <richo@psych0tik.net>
2015-01-05 15:38:20 -0800

    refactor argument parsing

--- src/multitooth.rs
+++ src/multitooth.rs
@@ -1,11 +1,15 @@
 #![crate_name = "multitooth"]
 #![feature(slicing_syntax)]
 
+extern crate getopts;
+
 use std::os;
 use std::io::stdio;
 use std::io::{IoErrorKind,LineBufferedWriter};
 use std::io::process::Command;
 use std::thread::{Thread,JoinGuard};
+
+use getopts::{optopt,optflag,getopts,OptGroup,usage};
 
 fn watch_ubertooth(cmd: String, mut args: Vec<String>, ubertooth: uint) {
     let mut stdout: LineBufferedWriter<_> = stdio::stdout();
@@ -47,28 +51,89 @@ fn watch_ubertooth(cmd: String, mut args: Vec<String>, ubertooth: uint) {
     }
 }
 
-fn main() {
-    // Jank, fix this later
-    let ubertooths: uint = match os::getenv("UBERTOOTHS") {
-        Some(s) => {
-            match s.parse() {
-                Some (i) => i,
-                None => panic!("Must supply UBERTOOTHS"),
-            }
-        },
-        None => panic!("Must supply UBERTOOTHS"),
-    };
-
+fn get_args() -> (Vec<String>, Vec<String>) {
     let args = os::args();
 
-    if args.len() == 1 {
-        panic!("TODO usage()");
+    let mut thru = false;
+    let mut parseargs: Vec<String> = vec![];
+    let mut thruargs: Vec<String> = vec![];
+
+    for a in args.iter() {
+        if a.as_slice() == "--" {
+            thru = true;
+        } else {
+            if thru {
+                thruargs.push(a.clone());
+            } else {
+                parseargs.push(a.clone());
+            }
+        }
+    }
+    return (parseargs, thruargs);
+}
+
+struct Opts {
+    ubertooths: uint,
+}
+
+fn parse_opts(args: Vec<String>, opts: &[OptGroup]) -> Option<Opts> {
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(f) => panic!(f),
+    };
+
+    if matches.opt_present("h") {
+        return None;
     }
 
-    let ref cmd = args[1];
-    let ref args = args[2..];
+    let ubertooths: uint = match matches.opt_str("n") {
+        Some(n) => match n.parse() {
+            Some (i) => i,
+            None => return None,
+        },
+        None => return None,
+    };
 
-    range(0, ubertooths).map(|i| -> JoinGuard<_> {
+    return Some(Opts {
+        ubertooths: ubertooths,
+    })
+}
+
+fn print_usage(opts: &[OptGroup], msg: Option<&str>) {
+    let ref program = os::args()[0];
+    let brief = format!("Usage: {} [options] -- ubertooth-<tool> [uberooth options]", program);
+    print!("{}", usage(brief.as_slice(), opts));
+
+    if let Some(s) = msg {
+        println!("{}", s);
+    }
+}
+
+fn main() {
+    let opts = &[
+        optopt("n", "", "number of ubertooths", "UBERTOOTHS"),
+        optflag("h", "help", "print this help menu"),
+    ];
+
+    let (parseargs, thruargs) = get_args();
+
+    let options = match parse_opts(parseargs, opts) {
+        Some(o) => o,
+        None => {
+            print_usage(opts, Some("-n is required"));
+            return;
+        }
+    };
+
+    if thruargs.len() == 0 {
+        print_usage(opts, Some("Must supply an ubertooth cmd"));
+        return;
+    }
+
+    let ref cmd = thruargs[0];
+    let ref args = thruargs[1..];
+
+    range(0, options.ubertooths).map(|i| -> JoinGuard<_> {
         let args = args.to_vec();
         let cmd = cmd.to_string();
 
